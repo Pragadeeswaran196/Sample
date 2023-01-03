d7fe062d62c87c52bc251940cb2e7d1df973754d
Pragadeeswaran196 <101637224+Pragadeeswaran196@users.noreply.github.com>
2023-01-03 13:11:10 +0530

    Update testing1.py

--- testing1.py
+++ testing1.py
@@ -1,4 +1,10 @@
+"""
 import os
 hpa_list=os.system("kubectl get hpa --all-namespaces")
 print("hpa_list")
 print("Hi")
+"""
+import subprocess
+
+# Get the list of HPAs for all namespaces
+hpas=subprocess.run(["kubectl", "get", "hpa", "--all-namespaces"])
